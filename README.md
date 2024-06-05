5b25c386ffb984a675b82eeea94995151035fc48
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2024-06-05 11:55:49 -0700

    Add example and update readme

--- README.md
+++ README.md
@@ -1,15 +1,40 @@
-# Gifify 
+# Gifify
+
 Make a gif out of a loop of `matplotlib` plots.
 
-Example:
+# Install
+
+`pip install gifify`
+
+# Example
+
 ```python
 import numpy as np
 import matplotlib.pyplot as plt
 from gifify import gifify
 
-x = np.linspace(0, 2*np.pi, 100)
+x = np.linspace(-np.pi, np.pi, 1000)
+shift = np.linspace(0, 2*np.pi, 50)[:-1]
 
-for i in gifify(range(100)):
-    plt.plot(x, np.sin(x + i/10))
+for s in gifify(shift, filename="sin.gif"):
+    plt.title(f"Shift: {s:.2f}")
+    plt.plot(x, np.sin(x + s))
 
 ```
+
+The above code creates the following gif:
+
+![sin](sin.gif)
+
+# Jupyter notebook support
+
+The created gif is displayed in the notebook if the code is run in a Jupyter notebook.
+This can be turned off by setting `display=False` in the `gifify` function.
+
+![example](example.png)
+
+# TODO
+
+-   [ ] Fix issue where the gif doesnt loop when displayed in the notebook
+-   [ ] Add controls to the gif in the notebook
+-   [ ] Add support for other formats beyond gifs
--- gifify/gifify.py
+++ gifify/gifify.py
@@ -1,18 +1,62 @@
 import matplotlib.pyplot as plt
+from IPython.display import Image, display
+from io import BytesIO
 import imageio
+from collections.abc import Iterable
 
 
-def gifify(iterator, filename="out.gif", temp_filename="temp.png"):
-    images = []
-    it = iter(iterator)
-    # check if the iterator is empty
-    while True:
-        try:
-            thing = next(it)
-            yield thing
-        except StopIteration:
-            imageio.mimsave(filename, images)
-            return
-        plt.savefig(temp_filename)
-        plt.close()
-        images.append(imageio.imread(temp_filename))
+# %%
+class Gifify(Iterable):
+    def __init__(self, iterator, filename="out.gif", display=True):
+        self.iterator = iterator
+        self.filename = filename
+        self.images = []
+        self.counter = 0
+        self.it = iter(iterator)
+        self.display = display
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.counter > 0:
+            b = BytesIO()
+            plt.savefig(b, format="png")
+            b.seek(0)
+            img = plt.imread(b)
+            img = (img * 255).astype("uint8")
+            b.close()
+            del b
+            self.images.append(img)
+            plt.close()
+
+        thing = next(self.it)
+        self.counter += 1
+        return thing
+
+    def __del__(self):
+        self.cleanup()
+
+    def cleanup(self):
+        imageio.mimsave(self.filename, self.images)
+        if self.display:
+            display(Image(self.filename))
+
+
+# %%
+def gifify(
+    iterator,
+    filename="out.gif",
+    display=True,
+):
+    """gifify.
+
+    :param iterator: iterable
+    :param filename: the filename to save the gif to
+    :param display: whether to display the gif in the notebook after the loop
+    """
+    return Gifify(
+        iterator,
+        filename=filename,
+        display=display,
+    )
