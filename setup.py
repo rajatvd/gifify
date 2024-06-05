7f8ec9a0a456a88eddc3005b5250a6ebea355754
Rajat Vadiraj Dwaraknath <rajatvd@gmail.com>
2024-06-05 11:57:32 -0700

    Update requirements and bump version

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="gifify",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     install_requires=[
         "imageio",
         "matplotlib",
+        "ipython",
     ],
     long_description="Wrap an iterator to make a gif out of matplotlib plots",
 )
